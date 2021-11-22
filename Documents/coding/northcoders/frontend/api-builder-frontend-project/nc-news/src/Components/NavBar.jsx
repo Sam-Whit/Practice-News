de69a8173d5aedf7379d9daca606624d5eabe04d
samwhit <sam_whitworth@hotmail.com>
2021-11-22 17:06:27 +0000

    nav bar progress

--- Documents/coding/northcoders/frontend/api-builder-frontend-project/nc-news/src/Components/NavBar.jsx
+++ Documents/coding/northcoders/frontend/api-builder-frontend-project/nc-news/src/Components/NavBar.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import {useEffect, useState} from "react";
 import{Link} from "react-router-dom";
+import { gettopics } from "./Utils/ApiCalls";
 
 const NavBar = () => {
-    return <div className='NavBar'>
-        <h2>NavBar</h2>
-    </div>
+    const [topics, settopics] = useState([]);
+
+    useEffect(() =>{
+        gettopics().then((topics) => {
+        settopics(topics)
+        })
+    }, []);
+
+    console.log(topics);
+    return <nav className='NavBar'>
+        {/* {articles.map((topic) => {
+            return <Link to={""}></Link>
+        }) } */}
+    </nav>
 };
 
 export default NavBar;
--- Documents/coding/northcoders/frontend/api-builder-frontend-project/nc-news/src/Components/Utils/ApiCalls.js
+++ Documents/coding/northcoders/frontend/api-builder-frontend-project/nc-news/src/Components/Utils/ApiCalls.js
@@ -3,3 +3,7 @@ import axios from "axios";
 const newsApi = axios.create({
   baseURL: "https://practice-news.herokuapp.com/api",
 });
+
+export function gettopics() {
+  return newsApi.get("/topics");
+}
