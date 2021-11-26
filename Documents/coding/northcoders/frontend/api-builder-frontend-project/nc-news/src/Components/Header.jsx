7456d0a6bba9d4ab54c276fe9e889d39694ba198
samwhit <sam_whitworth@hotmail.com>
2021-11-26 09:39:02 +0000

    Logged in user display and Log out button

--- Documents/coding/northcoders/frontend/api-builder-frontend-project/nc-news/src/Components/Header.jsx
+++ Documents/coding/northcoders/frontend/api-builder-frontend-project/nc-news/src/Components/Header.jsx
@@ -1,8 +1,24 @@
 import {Link} from "react-router-dom";
+import { useContext, useState } from "react";
+import { UserContext } from "../Contexts/userContext";
 
 const Header = (setQuery) => {
+    const { user, logOut, isLoggedIn } = useContext(UserContext);
+
+    const HideLoggedInAs = ({children}) => {
+        return (
+            <>
+              {isLoggedIn && children}
+            </>
+          );
+    }
 return (
     <header className='headerContainer'>
+        <HideLoggedInAs>
+        <p> Logged in as: {user.username}</p>
+        <button onClick={() =>{logOut()}}>Log Out</button>
+        </HideLoggedInAs>
+
         <Link to="/" onClick={(()=>setQuery(""))}>
             <h1>Practice News</h1>
         </Link>
